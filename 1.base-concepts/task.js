ca58a6d6fa1fa4106df6f43d2f2a01b49fb5b5d7
Regina <rmyachina@mail.ru>
2023-03-01 21:52:02 +0300

    задание 1 выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,17 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discr = Math.pow(b, 2) - 4 * a * c;
+  if (discr < 0) {
+    return arr;
+  } else if (discr > 0) {
+    let firstResult = (-b + Math.sqrt(discr) ) / (2 * a);
+    let secondResult = (-b - Math.sqrt(discr) )/(2 * a);
+    arr.push(firstResult, secondResult);
+  } else if (discr === 0) {
+    let result = -b/(2*a);
+    arr.push(result);
+  }
   return arr;
 }
 
