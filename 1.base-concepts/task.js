c9f0350eef912bc5fa67a9ff2faa1cfede899cf3
Regina <rmyachina@mail.ru>
2023-03-03 16:02:54 +0300

    изменения после доработки

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,7 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let discr = Math.pow(b, 2) - 4 * a * c;
-  if (discr < 0) {
-    return arr;
-  } else if (discr > 0) {
+  if (discr > 0) {
     let firstResult = (-b + Math.sqrt(discr) ) / (2 * a);
     let secondResult = (-b - Math.sqrt(discr) )/(2 * a);
     arr.push(firstResult, secondResult);
@@ -16,13 +14,13 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (!isNaN(percent) && !isNaN(contribution) && !isNaN(amount) && !isNaN(countMonths)) {
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  } else {
     let percentPerMounth  = percent / 100 / 12;
     let creditBody = amount - contribution;
     let paymentPerMounth = creditBody * (percentPerMounth + (percentPerMounth / (Math.pow((1 + percentPerMounth), countMonths)  - 1)));
     let totalMortgage = paymentPerMounth * countMonths;
-    return totalMortgage.toFixed(2);
-  } else {
-    return false;
+    return + totalMortgage.toFixed(2);
   }
 }
