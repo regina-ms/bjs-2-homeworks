01666b693a7b48227c8a2bd6701647f076d62b5d
Regina <rmyachina@mail.ru>
2023-03-09 16:06:30 +0300

    попытка 1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,27 @@
-function compareArrays(arr1, arr2) {
-  
-}
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+
+function compareArrays(arr1, arr2) {
+    if(arr1.length === arr2.length) {
+      return arr1.every ((item, index) => item === arr2[index]);
+    } else {
+      return false
+    }
+  }
+
+  function getUsersNamesInAgeRange(users, gender) {
+    if(users.length === 0) {
+        return 0
+    } else { 
+        let filteredUsers = users.filter(user => user.gender === gender);
+        if (filteredUsers.length === 0) {
+          return 0
+        } else {
+          let filteredUsersAge = filteredUsers.map(user => user.age);
+          let averageAge = filteredUsersAge.reduce((prev, item) => {
+            return prev + item;
+          }) / filteredUsersAge.length;
+        
+          return averageAge;
+      }
+    }
+  }
