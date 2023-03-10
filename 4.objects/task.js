01c865fbdd473c9b653ff01c934b4961f67e874e
Regina <rmyachina@mail.ru>
2023-03-10 15:06:36 +0300

    попытка 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
+'use strict'
+
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(this.marks) {
+      this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(!this.marks) {
+      return 0
+  } else {
+      return this.marks
+      .reduce((prev, item, index, array) => prev + item / array.length, 0);
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
