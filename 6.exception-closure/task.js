47a7174d7dae60af2f9f99d20ec49023d3cce194
Regina <rmyachina@mail.ru>
2023-03-18 16:15:03 +0300

    Задание 1, попытка 1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,22 @@
-﻿
+﻿'use strict'
+
+function parseCount(value) {
+  try{
+    if(isNaN(Number.parseFloat(value))) {
+      throw new Error("Невалидное значение");
+    }
+  } catch(error) {
+    return error;
+  }
+  
+  return Number.parseFloat(value);
+}
+
+function validateCount(valueToPars) {
+  try{
+    parseCount(valueToPars);
+  } catch (error) {
+    return error;
+  }
+  return parseCount(valueToPars);
+}
