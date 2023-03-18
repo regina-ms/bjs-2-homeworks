ff1f2ea7764bea13f677fcf1d718914ff5d2302e
Regina <rmyachina@mail.ru>
2023-03-18 18:26:49 +0300

    Задание 2, попытка 1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -16,3 +16,43 @@ function validateCount(valueToPars) {
   }
   return parseCount(valueToPars);
 }
+
+class Triangle {
+    constructor(a, b, c){
+      if(a + b < c || a + c < b || b + c < a) {
+        throw new Error ("Треугольник с такими сторонами не существует");
+      }
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+    get perimeter(){
+      return this.a + this.b + this.c;
+    }
+  
+    get area() {
+      let p = 0.5 * this.perimeter;
+      return Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3)
+    }
+  }
+  
+  class NullTriangle extends Triangle {
+    constructor(a, b, c){
+      super(a, b, c)
+    }
+    get perimeter(){
+      return "Ошибка! Треугольник не существует";
+    }
+  
+    get area() {
+      return "Ошибка! Треугольник не существует";
+    }
+  }
+  
+  function getTriangle (a, b, c) {
+    try {
+      return new Triangle (a, b, c);
+    } catch (error) {
+      return new NullTriangle;
+    }
+  }
