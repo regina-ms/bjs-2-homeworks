3ae35bb3391599c6c84a70925689776a79c8ca79
Regina <rmyachina@mail.ru>
2023-03-25 18:12:55 +0300

    Попытка 2

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,64 +1,62 @@
 class AlarmClock {
-    constructor(alarmCollection, intervalId) {
-      this.alarmCollection = [];
-      this.intervalId = null;
+  constructor(alarmCollection, intervalId) {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+  addClock(timeToStatrt, func){
+    if(!timeToStatrt || !func) {
+      throw new Error("Отсутствуют обязательные аргументы");
     }
-    addClock(timeToStatrt, func){
-      if(!timeToStatrt || !func) {
-        throw new Error("Отсутствуют обязательные аргументы");
+      if (this.alarmCollection.find(item => item.time === timeToStatrt)){
+      console.warn('Уже присутствует звонок на это же время');
+    }
+  
+   let obj =  {
+        callback: func,
+        time: timeToStatrt,
+        canCall: true,
+    }
+
+    this.alarmCollection.push(Object.create(obj));
+  }
+
+  removeClock(time){
+    let clocksToRemove = this.alarmCollection.filter(item => item.time === time);
+    for(let item of clocksToRemove){
+      if(this.alarmCollection.includes(item)){
+        this.alarmCollection.splice(this.alarmCollection.indexOf(item), 1)
       }
-        if (this.alarmCollection.find(item => item.time === timeToStatrt)){
-        console.warn('Уже присутствует звонок на это же время');
-      }
-    
-      class Ring {
-        constructor(callback, time, canCall){
-          this.callback = func;
-          this.time = timeToStatrt;
-          this.canCall = true;
-        }
-      }
-  
-      this.alarmCollection.push(new Ring);
-    }
-  
-    removeClock(time){
-      let clocksToRemove = this.alarmCollection.filter(item => item.time === time);
-      for(let item of clocksToRemove){
-        if(this.alarmCollection.includes(item)){
-          this.alarmCollection.splice(this.alarmCollection.indexOf(item), 1)
-        }
-      }
-    }
-  
-    getCurrentFormattedTime(){
-      return new Date().toLocaleTimeString().slice(0,-3);
-    }
-  
-    start(){
-      if(this.intervalId) {
-        return;
-      }
-  
-      this.intervalId = setInterval(this.alarmCollection.forEach(item => {
-        if(item.time === this.getCurrentFormattedTime() && item.canCall === true) {
-          item.canCall = false;
-          item.callback();
-        }
-      }), 1000)
-    }
-  
-    stop() {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
-    }
-  
-    resetAllCalls(){
-      this.alarmCollection.forEach(item => item.canCall === true);
-    }
-  
-    clearAlarms() {
-      this.stop();
-      this.alarmCollection = [];
     }
   }
+
+  getCurrentFormattedTime(){
+    return new Date().toLocaleTimeString().slice(0,-3);
+  }
+
+  start(){
+    if(this.intervalId) {
+      return;
+    }
+
+    this.intervalId = setInterval(this.alarmCollection.forEach(item => {
+      if(item.time === this.getCurrentFormattedTime() && item.canCall === true) {
+        item.canCall = false;
+        item.callback();
+      }
+    }), 1000)
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls(){
+    this.alarmCollection.forEach(item => item.canCall = true);
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
