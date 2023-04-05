f487f15b0623e116e78e49f09fd07c2d0ab342cb
Regina <rmyachina@mail.ru>
2023-04-05 22:54:27 +0300

    Попытка 1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,52 @@
 //Задача № 1
-function cachingDecoratorNew(func) {
-  
-}
+    function cachingDecoratorNew(func) {
+        let cache = [];
+        const maxCacheValuesCount = 5;
+        return (...args) => {
+            let hash = md5(args); 
+            let objectInCache = cache.find(item => hash in item);
+            if(objectInCache) {
+                return "Из кэша: " + objectInCache[hash];
+            }
+            
+            let result = func(...args);
+            let obj = {};
+            obj[hash] = result; 
+            cache.push(obj);
+           
+            if(cache.length > maxCacheValuesCount) {
+                cache.splice(0,1);
+            }
+            return "Вычисляем: " + result;
+        }
+    }
+    
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
   
+    function wrapper(...args) {
+      wrapper.allCount++;
+  
+      if(timeoutId) {
+        clearTimeout(timeoutId);
+      } else if(!timeoutId && !wrapper.count){
+        func(...args);
+          clearTimeout(timeoutId);
+          wrapper.count++;
+          
+      }
+      
+      timeoutId = setTimeout(()=> {
+          func(...args);
+          clearTimeout(timeoutId);
+          wrapper.count++;
+      }, delay);
+  
+    }
+    
+    return wrapper;
 }
